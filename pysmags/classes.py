5adabbd39b9b3dedbc7f742905f14de686c9f79d
frostysmags <frostysmags+dev@gmail.com>
2023-02-09 01:37:51 -0500

    fixed ctrl+c exit bug with QThread

--- pysmags/classes.py
+++ pysmags/classes.py
@@ -2,6 +2,11 @@ import threading, queue, time, signal, sys
 
 class QThread:
     def __init__(self, *args:list[callable], **kwargs) -> None:
+        '''
+        use one of the following kwargs for debugging\n
+        debug=True # Allows selective debugging - functions return a debug str\n
+        debug=[True, True] # Displays all debugs by default\n
+        '''
         self.__debugMode__ = False
         self.__displayDebug__ = False
         self.__running__ = True
@@ -10,7 +15,6 @@ class QThread:
         self.__threadArgs__ = args
         self.__threadKWArgs__ = kwargs
         self.threadName = args[0].__name__
-        signal.signal(signal.SIGINT, self.__handle_sigint__)
         if 'loop' in kwargs and kwargs['loop'] == True:
             self.__threadCore__ = threading.Thread(target=self.__loopThreadFunction__)
         else:
@@ -20,6 +24,7 @@ class QThread:
         if 'debug' in kwargs and kwargs['debug'] == [True, True]:
             self.__debugMode__ = True
             self.__displayDebug__ = True
+        signal.signal(signal.SIGINT, self.__handle_sigint__)
         
     def clearQueue(self) -> str:
         print(f"Cleared Queue of Thread Running '{self.threadName}'\n" if self.__debugMode__ and self.__displayDebug__ else "", end="")
@@ -27,7 +32,7 @@ class QThread:
         return f"Cleared Queue of Thread Running '{self.threadName}'" if self.__debugMode__ else None
 
     def start(self) -> str:
-        print(f"Starting Thread Running '{self.threadName}\n'" if self.__debugMode__ and self.__displayDebug__ else "", end="")
+        print(f"Starting Thread Running '{self.threadName}'\n" if self.__debugMode__ and self.__displayDebug__ else "", end="")
         self.__threadCore__.start()
         return f"Starting Thread Running '{self.threadName}'" if self.__debugMode__ else None
 
@@ -41,7 +46,7 @@ class QThread:
     def __handle_sigint__(self, signum, frame):
         self.__running__ = False
         self.threadQueue.queue.clear()
-        sys.exit()
+        exit(0)
 
     def __threadFunction__(self) -> None:
         self.__threadArgs__[0]()
@@ -60,8 +65,9 @@ class QThread:
 class TimeThread:
     def __init__(self, **kwargs) -> None:
         '''
-        use one of the following kwargs if applicable\n
-        debug=True # Allows debugging with selective debugging\n
+        A Thread that runs in the background keeping track of how long the thread's been active\n
+        use one of the following kwargs for debugging\n
+        debug=True # Allows selective debugging - functions return a debug str\n
         debug=[True, True] # Displays all debugs by default\n
         '''
         self.__mainThread__ = QThread(self.timeTrack, loop=True, **kwargs)
@@ -77,19 +83,12 @@ class TimeThread:
     
     def timeTrack(self):
         time.sleep(1)
-        # self.__clock__['seconds'] += 1
-        # if self.__clock__['seconds'] % 60:
-        #     self.__clock__['seconds'] = 0
-        #     self.__clock__['minutes'] += 1
-        # if self.__clock__['minutes'] % 60:
-        #     self.__clock__['minutes'] = 0
-        #     self.__clock__['hours'] += 1
-        self.clock['seconds'] += 1
-        self.clock['minutes'] += (self.clock['seconds'] == 60)
-        self.clock['hours'] += (self.clock['minutes'] == 60)
-        self.clock['minutes'] %= 60
-        self.clock['seconds'] %= 60
-    
+        self.__clock__['seconds'] += 1
+        self.__clock__['minutes'] += (self.__clock__['seconds'] == 60)
+        self.__clock__['hours'] += (self.__clock__['minutes'] == 60)
+        self.__clock__['minutes'] %= 60
+        self.__clock__['seconds'] %= 60
+
     def active(self):
         return f'[{"{:02d}".format(self.__clock__["hours"])}:{"{:02d}".format(self.__clock__["minutes"])}:{"{:02d}".format(self.__clock__["seconds"])}]'
 
